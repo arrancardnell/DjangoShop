c4bc4918c5c4f138ff3fa216cb50eebd2e1f6409
arrancardnell <arrancardnell@gmail.com>
2017-03-14 21:08:22 +0000

    Added Payment processing views and templates.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'cart',
     'orders',
     'paypal.standard.ipn',
+    'payment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
